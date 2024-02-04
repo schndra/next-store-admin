ffc2e765ac9e79f61feaf69d8163767923c95c5f
schndra <ardnihc@gmail.com>
2024-02-04 21:36:24 +0530

    feat: add SiteHeader component

---
+++ components/SiteHeader.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/assets/logo.png";
+import { MobileNav } from "./MobileNav";
+import ThemeToggle from "./ThemeToggle";
+
+function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
+        <div className="mr-4 hidden md:flex">
+          <Link href="/" className="mr-6 flex items-center space-x-2">
+            <Image src={Logo} alt="log" width={24} height={24} />
+            <span className="hidden font-bold sm:inline-block">Store</span>
+          </Link>
+        </div>
+        <MobileNav />
+        <div className="flex items-center gap-x-4 ">
+          <ThemeToggle />
+          <h1>user icon</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default SiteHeader;
