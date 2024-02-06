cf0305d8ed0f4fdc73d0a0d7ae86229c01f817a5
schndra <ardnihc@gmail.com>
2024-02-06 20:41:26 +0530

    feat: complete page layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,24 +1,31 @@
+import Footer from "@/components/Footer";
 import { Sidebar } from "@/components/Sidebar";
 import SiteHeader from "@/components/SiteHeader";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { PropsWithChildren } from "react";
 
 function layout({ children }: PropsWithChildren) {
   return (
-    <main className="grid lg:grid-cols-5">
-      <div className="grid col-span-5">
+    <div>
+      <div className="relative flex min-h-screen flex-col bg-background">
         <SiteHeader />
+        <main className="flex-1">
+          {/* page layout */}
+          <div className="border-b">
+            <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+              <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
+                <ScrollArea className="h-full py-6 pr-6 lg:py-8">
+                  <Sidebar />
+                </ScrollArea>
+              </aside>
+              {children}
+            </div>
+          </div>
+          {/* {children} */}
+        </main>
+        <Footer />
       </div>
-      {/*  hide on small screen */}
-      <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
-        <Sidebar />
-      </div>
-      <div className="lg:col-span-4">
-        <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
-      </div>
-      <div className="grid col-span-5">
-        <h1>footer</h1>
-      </div>
-    </main>
+    </div>
   );
 }
 export default layout;
