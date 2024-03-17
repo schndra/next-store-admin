0f0ddaf17404cd86272b35f887fb7b6dcff38cd7
schndra <ardnihc@gmail.com>
2024-03-18 01:21:04 +0530

    feat: add settings btn, display user email & name in UserBtn.tsx

--- components/auth/UserBtn.tsx
+++ components/auth/UserBtn.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import LogoutButton from "./LogoutButton";
+import { useCurrentUser } from "@/hooks/use-current-user";
 
 function UserBtn() {
-  const session = useSession();
-  const user = session.data?.user;
+  const user = useCurrentUser();
 
   return (
     <DropdownMenu>
@@ -23,10 +25,29 @@ function UserBtn() {
           <AvatarFallback>{user?.name?.split("")[0]}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <LogoutButton>
-          <DropdownMenuItem>Logout</DropdownMenuItem>
-        </LogoutButton>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{user?.name}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {user?.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          {/* <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem> */}
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <LogoutButton>
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </LogoutButton>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
