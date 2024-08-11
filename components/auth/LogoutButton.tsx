245f8d38d64bf82cea0a40dd7b559d8bc1462321
schndra <ardnihc@gmail.com>
2024-08-11 22:35:37 +0530

    fix: add logout component missing ref type

--- components/auth/LogoutButton.tsx
+++ components/auth/LogoutButton.tsx
@@ -1,16 +1,23 @@
-"use client";
-
+import React from "react";
 import { logoutAction } from "@/actions/logout";
 
-function LogoutButton({ children }: { children: React.ReactNode }) {
+const LogoutButton = React.forwardRef<
+  HTMLSpanElement,
+  { children: React.ReactNode }
+>((props, ref) => {
+  const { children } = props;
+
   const handleClick = () => {
     logoutAction();
   };
 
   return (
-    <span onClick={handleClick} className="cursor-pointer">
+    <span onClick={handleClick} className="cursor-pointer" ref={ref}>
       {children}
     </span>
   );
-}
+});
+
+LogoutButton.displayName = "LogoutButton"; // Required for display name in debugging
+
 export default LogoutButton;
