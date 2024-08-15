87d6e7ec3c5218872f0ccb14d8b6accbda190553
schndra <ardnihc@gmail.com>
2024-08-16 00:09:48 +0530

    feat: add featured productlist and new prod list to home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,41 @@ import Logo from "@/assets/logo.png";
 import Navbar from "@/components/store/Navbar";
 import Footer from "@/components/store/Footer";
 import Slider from "@/components/store/Slider";
+import ProductList from "@/components/store/ProductList";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Slider />
+      <div className="">
+        <Slider />
+        <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          <h1 className="text-2xl">Featured Products</h1>
+          {/* <Suspense fallback={<Skeleton />}> */}
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
+            limit={4}
+          />
+          {/* </Suspense> */}
+        </div>
+        <div className="mt-24">
+          <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+            Categories
+          </h1>
+          {/* <Suspense fallback={<Skeleton />}>
+          <CategoryList />
+        </Suspense> */}
+        </div>
+        <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          <h1 className="text-2xl">New Products</h1>
+          {/* <Suspense fallback={<Skeleton />}> */}
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
+            limit={4}
+          />
+          {/* </Suspense> */}
+        </div>
+      </div>
       <Footer />
     </>
     // <main className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-primary-foreground to-slate-950">
---
+++ components/store/ProductList.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const PRODUCT_PER_PAGE = 8;
+
+const ProductList = async ({
+  categoryId,
+  limit,
+  searchParams,
+}: {
+  categoryId: string;
+  limit?: number;
+  searchParams?: any;
+}) => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href={"/test"}
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/27163415/pexels-photo-27163415/free-photo-of-residential-building-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src={"/product.png"}
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">product name</span>
+          <span className="font-semibold">LKR 200</span>
+        </div>
+
+        <div className="text-sm text-gray-500"> my desc</div>
+
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
