10a2c9e3f172e28d098e0b1350a52e14f8c5092d
schndra <ardnihc@gmail.com>
2024-08-15 23:51:34 +0530

    feat: add pexels in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
       },
       {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
       },
