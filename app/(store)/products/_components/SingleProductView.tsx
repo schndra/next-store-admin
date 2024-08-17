2f368b5fb33fb37987cd1e3a9d59bc6cd6960a94
schndra <ardnihc@gmail.com>
2024-08-17 20:32:22 +0530

    feat : add product info , refund policy & shipping info section in single product page

--- app/(store)/products/_components/SingleProductView.tsx
+++ app/(store)/products/_components/SingleProductView.tsx
@@ -39,8 +39,35 @@ function SingleProductView({ productId }: { productId: string }) {
         <div className="h-[2px] bg-gray-100" />
 
         <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>sec desc</p>
+          <h4 className="font-medium mb-4">PRODUCT INFO</h4>
+          <p>
+            The Men's Classic White and Grey Hoodie offers a timeless look with
+            a comfortable fit. Made from a soft blend of cotton and polyester,
+            this hoodie is perfect for casual outings or lounging at home. The
+            adjustable drawstring hood and kangaroo pocket add functionality and
+            style, making it a versatile piece in your wardrobe.
+          </p>
+        </div>
+        <div className="text-sm mt-1">
+          <h4 className="font-medium mb-4">RETURN & REFUND POLICY</h4>
+          <p>
+            We want you to be completely satisfied with your purchase. If you
+            are not happy with your Men's Classic White and Grey Hoodie, you may
+            return it within 30 days of delivery for a full refund or exchange.
+            The product must be in its original condition, unworn, and with all
+            tags attached. Please note that shipping fees are non-refundable.
+          </p>
+        </div>
+
+        <div className="text-sm mt-1">
+          <h4 className="font-medium mb-4">SHIPPING INFO</h4>
+          <p>
+            Enjoy fast and reliable shipping on your order. We process and ship
+            all orders within 2-3 business days. Standard shipping typically
+            takes 5-7 business days, while express options are available at
+            checkout for faster delivery. You'll receive a tracking number as
+            soon as your Men's Classic White and Grey Hoodie is on its way.
+          </p>
         </div>
 
         <div className="h-[2px] bg-gray-100" />
